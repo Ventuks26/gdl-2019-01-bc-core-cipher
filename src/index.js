3b443a35e25b4092c0a854a181a2e232bce00063
Ventura <mateventura5@gmail.com>
2019-01-30 13:35:28 -0600

    funcion para cifrar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 
-const numDesp = $('#txNumero').val();
-const numDesp = $('#txNombre').val();
+const numDesp = document.getElementById('txNumero').value;
+const nombr = document.getElementById('txNombre').value;
+document.getElementById('cif').addEventListener("click", cifrar);
 
-function cifrar() {
+  function cifrar(nombr, numDesp) {
+
+let encode= "";//variable vacia para poder guardar lo que va codificando
+console.log(nombr,nombr.length);
+       for(let i= 0; i <nombr.length; i++){
+const nombr= text.CharCodeAt(i);// traduce las letras a ascii
+
+        if(65 <= nombr && nombr <= 90) {
+          encode += string.formCharCode(((nombr-65+numDesp)) %26)+65
+        }
+          else{
+            encode+=nombr.charAt(i);
+          }
+}
+    return encode;
+
+
+
 
 }
-
-function descifrar(){
-  
-}
+console.log(cifrar(nombr, numDesp));
