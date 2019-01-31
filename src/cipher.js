2e55a646de83d7bda689d4f2e83eb67258b01a3d
Ventura <mateventura5@gmail.com>
2019-01-31 13:25:36 -0600

    funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,38 @@
 window.cipher = {
-  // ... 
+encode: function  cf (numDesp, nombr) {
+
+    let nomCif= [];
+
+        for(let i=0; i < nombr.length; i++){
+          let caract= nombr[i];
+          let comAscii= caract.charCodeAt(0);
+          //console.log(comAscii);
+          let codificando = (comAscii - 65 + numDesp) % 26 + 65;
+          let convertir= String.fromCharCode(codificando);
+          nomCif.push(convertir);
+        //console.log(nomCif);
+
+      }
+      return nomCif;
+    },
+
+  decode: function df(numDesp, nombr) {
+
+      let nomDecif= [];
+
+          for(let i=0; i < nombr.length; i++){
+            let caract= nombr[i];
+            let comAscii= caract.charCodeAt(0);
+            //console.log(comAscii);
+            let decodificando = (comAscii + 65 -numDesp) % 26 + 65;
+            let convertir= String.fromCharCode(decodificando);
+            nomDecif.push(convertir);
+          //console.log(nomCif);
+
+        }
+        return nomDecif;
+      },
+
+
+  // ...
 };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 
-const numDesp = document.getElementById('txNumero').value;
-const nombr = document.getElementById('txNombre').value;
-document.getElementById('cif').addEventListener("click", cifrar);
-
-  function cifrar(nombr, numDesp) {
-
-let encode= "";//variable vacia para poder guardar lo que va codificando
-console.log(nombr,nombr.length);
-       for(let i= 0; i <nombr.length; i++){
-const nombr= text.CharCodeAt(i);// traduce las letras a ascii
-
-        if(65 <= nombr && nombr <= 90) {
-          encode += string.formCharCode(((nombr-65+numDesp)) %26)+65
-        }
-          else{
-            encode+=nombr.charAt(i);
-          }
+funtion cifrar () =>{
+  const numDesp = document.getElementById('txNumero').value;
+  const nombr = document.getElementById('txNombre').value;
+  document.getElementById("result").value= window.cipher.encode(numDesp, nombr);
 }
-    return encode;
 
-
-
-
-}
-console.log(cifrar(nombr, numDesp));
+  document.getElementById("cif").addEventListener("click", cifrar);
