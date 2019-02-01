73d04cac6da6ded526d74ce5b6eb32744c407e19
Ventuks26 <47006491+Ventuks26@users.noreply.github.com>
2019-01-31 22:38:27 -0600

    ¨color¨

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 
-funtion cifrar () =>{
+funtion cifrar () {
   const numDesp = document.getElementById('txNumero').value;
   const nombr = document.getElementById('txNombre').value;
   document.getElementById("result").value= window.cipher.encode(numDesp, nombr);
 }
 
   document.getElementById("cif").addEventListener("click", cifrar);
+
+
+  funtion decifrar () {
+    const numDesp = document.getElementById('txNumero').value;
+    const nombr = document.getElementById('txNombre').value;
+    document.getElementById("result").value= window.cipher.encode(numDesp, nombr);
+  }
+
+    document.getElementById("descifrar").addEventListener("click", decifrar);
