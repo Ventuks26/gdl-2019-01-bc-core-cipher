7d54fdf11752219880cd07d91cbe253257a2a4ea
Ventura <mateventura5@gmail.com>
2019-02-05 12:13:39 -0600

    terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,50 @@
 window.cipher = {
 encode: function  cf (numDesp, nombr) {
-  console.log(typeof numDesp + " "+ nombr );
+  let nombre= nombr.toUpperCase();
+  let arreglo= nombre.split(" ");
+  let nomCif= [];
 
-    let nomCif= [];
+   for (let j=0; j<arreglo.length; j++) {
+     let palabra= arreglo[j];
+     for(let i=0; i < palabra.length; i++){
+       let letra= palabra[i];
+       let comAscii= letra.charCodeAt(0);
+       let codificando = (comAscii - 65 +numDesp) % 26 + 65;
+       let convertir= String.fromCharCode(codificando);
+       nomCif.push(convertir);
 
 
-        for(let i=0; i < nombr.length; i++){
-          let comAscii= nombr.charCodeAt(i);
-          let codificando = ((comAscii - 65 + numDesp) % 26) + 65;
-          let convertir = String.fromCharCode(codificando);
-          nomCif.push(convertir);
-
-
-      }
-      return nomCif;
+     }
+     nomCif.push(" ");
+    }
+     let cadenaFinal= nomCif.join("");
+      return cadenaFinal.trim();
     },
 
   decode: function df(numDesp, nombr) {
 
+      let nombre= nombr.toUpperCase();
+      let arreglo= nombre.split(" ");
       let nomDescif= [];
 
-          for(let i=0; i < nombr.length; i++){
-            let comAscii= nombr.charCodeAt(i);
-            let decodificando = (comAscii + 65 -numDesp) % 26 + 65;
-            let convertir= String.fromCharCode(decodificando);
-            nomDescif.push(convertir);
+       for (let j=0; j<arreglo.length; j++) {
+         let palabra= arreglo[j];
+         for(let i=0; i < palabra.length; i++){
+           let letra= palabra[i];
+           let comAscii= letra.charCodeAt(0);
+           let decodificando = (comAscii + 65 -numDesp) % 26 + 65;
+           let convertir= String.fromCharCode(decodificando);
+           nomDescif.push(convertir);
 
 
+         }
+         nomDescif.push(" ");
         }
-        return nomDescif;
-      },
+         let cadenaFinal= nomDescif.join("");
+          return cadenaFinal.trim();
+        },
+
+
 
 
   // ...
